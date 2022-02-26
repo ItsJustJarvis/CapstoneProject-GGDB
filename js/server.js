da4ddb31813df9b3a2dfd1c4f24cd212eae637bf
Reeve Jarvis <reevejarvis@hotmail.com>
2022-02-26 12:06:39 -0800

    Create initial JS files

---
+++ js/server.js
@@ -0,0 +1,8 @@
+/*
+Author:     Reeve Jarvis
+Project:    GGDB-Good Game Database
+Filename:   server.js
+Updated:    02/26/2022
+*/
+
+"use strict";
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+/*
+Author:     Reeve Jarvis
+Project:    GGDB-Good Game Database
+Filename:   main.js
+Updated:    02/26/2022
+*/
+
+"use strict";
