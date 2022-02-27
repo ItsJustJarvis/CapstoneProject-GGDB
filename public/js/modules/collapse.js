bef23482aa23b0d9c8ec3f421f190880f1e33161
Reeve Jarvis <reevejarvis@hotmail.com>
2022-02-26 20:43:46 -0800

    Create collapse.js module and toggleForm function
    This module and function will be used to toggle visibility on the search filter form

---
+++ public/js/modules/collapse.js
@@ -0,0 +1,20 @@
+/*
+Author:     Reeve Jarvis
+Project:    GGDB-Good Game Database
+Filename:   collapse.mjs
+Updated:    02/26/2022
+*/
+
+"use strict";
+
+function toggleForm(){
+    let searchForm = document.querySelector(".search__form");
+    if(!searchForm.classList.contains("open")){
+        searchForm.classList.add("open");
+    } else {
+        searchForm.classList.remove("open");
+    }
+}
+
+
+export {toggleForm};
