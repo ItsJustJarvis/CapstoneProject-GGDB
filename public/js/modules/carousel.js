26c0f4058032a4629aa2b068c24954126b7c10fc
Reeve Jarvis <reevejarvis@hotmail.com>
2022-02-27 13:43:48 -0800

    Create carousel.js module for later use

---
+++ public/js/modules/carousel.js
@@ -0,0 +1,38 @@
+// Possible Carousel Example to use for later improvements: 
+// https://codepen.io/tuesta/pen/QoMqBY 
+// Pen: Carousel YoutubeMusic 
+// Author: victor tuesta ascoy 
+
+// Other examples: https://freefrontend.com/javascript-carousels/
+
+
+//NEEDS UPDATING TO FIT APP NEEDS AND MATCH STRUCTURE
+
+// const gap = 32;
+
+// const carousel = document.getElementById("results"),
+//     content = document.getElementById("content"),
+//     next = document.getElementById("next"),
+//     prev = document.getElementById("prev");
+
+// next.addEventListener("click", e => {
+//     carousel.scrollBy(width + gap, 0);
+//     if (carousel.scrollWidth !== 0) {
+//         prev.style.display = "flex";
+//     }
+//     if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+//         next.style.display = "none";
+//     }
+// });
+// prev.addEventListener("click", e => {
+//     carousel.scrollBy(-(width + gap), 0);
+//     if (carousel.scrollLeft - width - gap <= 0) {
+//         prev.style.display = "none";
+//     }
+//     if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+//         next.style.display = "flex";
+//     }
+// });
+
+// let width = carousel.offsetWidth;
+// window.addEventListener("resize", e => (width = carousel.offsetWidth));
