ecd1e2c91f9f5c8a122d863797e6870bfc54c6b7
Reeve Jarvis <reevejarvis@hotmail.com>
2022-02-26 21:05:51 -0800

    Prepare node/express app with server.js file

--- js/server.js
+++ js/server.js
@@ -6,3 +6,39 @@ Updated:    02/26/2022
 */
 
 "use strict";
+
+const express = require("express");
+const Datastore = require("nedb");
+const fetch = require("node-fetch");
+const app = express();
+require("dotenv").config();
+
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+});
+app.use(express.static("public"));
+app.use(express.json({ limit: "5mb" }));
+
+const database = new Datastore("game-list.db");
+database.loadDatabase();
+
+app.get("/api", (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+
+app.post("/api", (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
