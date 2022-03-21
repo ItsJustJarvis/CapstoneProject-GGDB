5bc3b7a6e70d9cb3b062d4732cb731225031fca5
Reeve Jarvis <reevejarvis@hotmail.com>
2022-03-21 16:47:13 -0700

    Create single carousel list request function to manage all lists

--- public/js/modules/request.js
+++ public/js/modules/request.js
@@ -8,3 +8,17 @@ Updated:    03/21/2022
 "use strict";
 
 
+async function getCarouselData(gameList) {
+    let games;
+    const requestDates = getRequestDates(gameList);
+    try {
+        const api_url = `/carousel/${requestDates}`;
+        const response = await fetch(api_url);
+        const json = await response.json();
+        games = json.results;
+        return games;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
