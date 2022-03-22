dd09ceb2d4741b2ee8a97663c851debe9acf3e23
Reeve Jarvis <reevejarvis@hotmail.com>
2022-03-22 13:39:15 -0700

    Add toggle form utility function

--- public/js/main.js
+++ public/js/main.js
@@ -94,7 +94,12 @@ async function populateGamePageContent(){
     }
 }
 
+function toggleForm(){
+    let searchForm = document.querySelector(".search__form");
+    if(!searchForm.classList.contains("open")){
+        searchForm.classList.add("open");
     } else {
+        searchForm.classList.remove("open");
     }
 }
 
